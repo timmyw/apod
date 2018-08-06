e2887816900dd1fcc375faf4bdafae458d57da72
Tim Whelan <tim.whelan@ravelin.com>
2018-08-06 09:37:42 +0100

    Debug output to track desktop session

--- getandsetapod.sh
+++ getandsetapod.sh
@@ -1,9 +1,27 @@
-#!/bin/sh
+#!/bin/zsh
+
+echo "START: " $UID $EUID $DESKTOP_SESSION
+
+desktop=mate
+
+case $desktop in
+    mate)
+        PID=$(pgrep mate-session)
+        export DBUS_SESSION_BUS_ADDRESS=$(grep -z DBUS_SESSION_BUS_ADDRESS /proc/$PID/environ|cut -d= -f2-)
+    ;;
+esac
 
 TMPDIR="$HOME/apod"
 mkdir -p $TMPDIR
 
+export PATH=$HOME/go/bin:$PATH
+echo $PATH
+
 IMGPATH=`apod --output $TMPDIR`
+echo $IMGPATH
+
+        dconf write  /org/mate/desktop/background/picture-filename "'$IMGPATH'"
+        dconf write  /org/mate/desktop/background/picture-options "'stretched'"
 
 case $DESKTOP_SESSION in
     mate)
@@ -13,3 +31,4 @@ case $DESKTOP_SESSION in
 esac
 
 
+echo "DONE" $DESKTOP_SESSION
