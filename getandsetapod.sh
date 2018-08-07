8ecc87c23f212545df5a5372cb29117b88377a01
Tim Whelan <tim.whelan@ravelin.com>
2018-08-07 16:24:20 +0100

    Working on handling for different desktops

--- getandsetapod.sh
+++ getandsetapod.sh
@@ -1,15 +1,19 @@
-#!/bin/zsh
+#!/bin/sh
 
 # Try to work out what desktop session we are running - this isn't
 # going to work unless X or Wayland is running
-PID=$(pgrep session) # Hopefully this finds the desktop session
-desktop=$(grep -z DESKTOP_SESSION  /proc/2734/environ|cut -d= -f2-)
 
+PID=$(pgrep session) # Hopefully this finds the desktop session
+desktop=$(grep -z DESKTOP_SESSION  /proc/$PID/environ|cut -d= -f2-)
+
+echo "desktop:[$desktop]:"
 case $desktop in
     mate)
         PID=$(pgrep mate-session)
         export DBUS_SESSION_BUS_ADDRESS=$(grep -z DBUS_SESSION_BUS_ADDRESS /proc/$PID/environ|cut -d= -f2-)
-    ;;
+        ;;
+    *)
+        echo "Unknown desktop:'$desktop'"
 esac
 
 TMPDIR="$HOME/apod"
